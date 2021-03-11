c7082a39c7bdfa9762c0f53a5030102c01846d67
Funny Gustanty Aryndhy <(funnyaryndhy@gmail.com)>
2021-03-11 22:36:38 +0700

    Pengerjaan Tugas

---
+++ I0320042_soal2_tugas2.py
@@ -0,0 +1,36 @@
+print("===== Identitas Pribadi =====")
+Nama_lengkap = "Funny Gustanty Aryndhy"
+Nama_panggilan = "Funny"
+Tempat_lahir = "Tulungagung"
+Tanggal_sekarang = 11
+Bulan_sekarang = 3
+Tahun_sekarang = 2021
+Tanggal_lahir = 15
+Bulan_lahir = 8
+Tahun_lahir = 2001
+Umur_bulan = ((Tanggal_sekarang - Tanggal_lahir)/30) + (Bulan_sekarang - Bulan_lahir) + ((Tahun_sekarang - Tahun_lahir)*12)
+Agama = "Islam"
+Jenis_kelamin = "Perempuan"
+Golongan_darah = "B"
+Alamat_rumah = "RT/RW 15/04, Sawo, Campurdarat, Tulungagung, Jawa Timur"
+Hobi = "membaca novel dan menonton drama"
+Cita_cita = "Pengusaha"
+Jumlah_saudara = "tiga"
+Kode_pos = 66272
+Berat_badan = "56.5 Kg"
+Tinggi_badan = "156 Cm"
+Ukuran_sepatu = "37 Cm"
+Pekerjaan = "mahasiswa"
+Kewarganegaraan = "Indonesia"
+Jurusan = "S-1 Teknik Industri"
+Fakultas = "Teknik"
+Angkatan = 2020
+Jalur_masuk = "Mandiri Satu"
+Perguruan_tinggi = "Universitas Sebelas Maret"
+Email = "funnyaryndhy@gmail.com"
+print("Hai! Perkenalkan namaku",Nama_lengkap,"\n","biasa dipanggil",Nama_panggilan,"\n","aku lahir di",Tempat_lahir,"\n","Umurku",Umur_bulan,"bulan","\n","agamaku",Agama,"\n","jenis kelaminku",Jenis_kelamin,"\n","golongan darahku",Golongan_darah,"\n","alamat rumahku",Alamat_rumah,"\n","hobiku",Hobi,"\n","cita-citaku ingin menjadi",Cita_cita,"\n","saudara kandungku berjumlah",Jumlah_saudara,"orang","\n","kode pos rumahku",Kode_pos,"\n","berat badanku",Berat_badan,"\n","tinggi badanku",Tinggi_badan,"\n","ukuran sepatuku",Ukuran_sepatu,"\n","aku adalah seorang",Pekerjaan,"\n","aku berkewarganegaraan",Kewarganegaraan,"\n","aku mengambil Jurusan",Jurusan,"\n","Fakultas",Fakultas,"\n","angkatan",Angkatan,"\n",Perguruan_tinggi,"\n","emailku",Email,"\n")
+
+
+
+
+
